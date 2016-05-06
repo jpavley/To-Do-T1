7cc9df8d8f7710c0866f942f43f7e2f50a3898b5
John Pavley <john@pavley.com>
2016-05-06 17:04:45 -0400

    initial checkin

--- To Do T1/FirstViewController.swift
+++ To Do T1/FirstViewController.swift
@@ -8,8 +8,12 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+var todoList:[String] = []
 
+class FirstViewController: UIViewController, UITableViewDelegate {
+
+    @IBOutlet weak var itemList: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +23,20 @@ class FirstViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todoList.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .Default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = todoList[indexPath.row]
+        return cell
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        itemList.reloadData()
+    }
 
 }
 
--- To Do T1/SecondViewController.swift
+++ To Do T1/SecondViewController.swift
@@ -8,18 +8,36 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var itemField: UITextField!
+    
+    @IBAction func addItem(sender: AnyObject) {
+        
+        todoList.append(itemField.text!)
+        
+        itemField.text = ""
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.itemField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 
 }
 
